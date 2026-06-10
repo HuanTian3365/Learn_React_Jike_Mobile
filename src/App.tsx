e29361c5d4bc32fb24ed17e3cf86673a3b089b76
HuanTian <HuanTian3365543554@outlook.com>
2026-06-10 19:34:03 +0800

    初始化

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-export default function App() { 
+import { Button } from "antd-mobile";
+export default function App() {
   return (
     <div>
       <h1>Ciallo App</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://geek.itheima.net",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
   plugins: [react()],
-})
+});
