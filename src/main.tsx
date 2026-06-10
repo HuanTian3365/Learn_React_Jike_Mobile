75ea6cc68a44317cdb6fd51b976645369f8877ab
HuanTian <HuanTian3365543554@outlook.com>
2026-06-10 19:37:50 +0800

    feat: 路由初始化

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
-import App from "./App";
-
-createRoot(document.getElementById("root")!).render(<App />
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />,
 );
